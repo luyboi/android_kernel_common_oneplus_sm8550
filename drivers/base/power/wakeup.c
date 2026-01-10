850d8c2041b1ba9a14f9c7e113032ee464ff154e
luyboi <237565436+luyboi@users.noreply.github.com>
2026-01-10 22:02:24 +0000

    Revert "power: wakeup: Fix suspend abort handling to prevent deadlock"
    This reverts commit c7db434ab8c4ea582e28814b248bc57371f2f346.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -972,11 +972,7 @@ bool pm_wakeup_pending(void)
 
 void pm_system_wakeup(void)
 {
-	int abort_count = atomic_inc_return_relaxed(&pm_abort_suspend);
-
-	suspend_abort_fs_sync();
-
-	if (abort_count == 1)
+	if (atomic_inc_return_relaxed(&pm_abort_suspend) == 1)
 		s2idle_wake();
 }
 EXPORT_SYMBOL_GPL(pm_system_wakeup);
